a4f5b9506b56efd1f71db7f5ddcda0fda5548fa9
Enas <enaselmahdwi@gmail.com>
2021-10-08 08:09:31 -0600

    done with design

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,19 +5,40 @@ import styled from 'styled-components';
 import '../App.css';
 function Character(props){
 const {dataDisplay}=props;
-console.log('dataDisplaynameeeeeeeeeeeeeeeeeeeeeee');
-console.log(dataDisplay.name);
 
+
+const StyledDetails  = styled.div`
+width:100%;
+height:100%;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+
+}
+
+
+div{
+   background-color:pink;
+   border:1px gray solid;
+   margin:1%;
+   padding:2%;
+   letter-spacing:3px;
+   text-align: center;
+   border-radius: 10px;
+   width:90%;
+}
+` 
 return (
-    <div >
-      <div></div>
-    {/*  data.map(da => { return <Character data={da}  key={da.id}/>})*/ }
+    < StyledDetails >
+     
+    
     <div>{dataDisplay.name}</div>
     
     
  
 
-    </div>
+    </StyledDetails>
   );
 }
 export default Character;
